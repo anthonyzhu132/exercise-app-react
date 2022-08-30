4c385988755a996094a2bfde4da85d8fa7960e2f
Anthony Zhu <anthonyzhu132@gmail.com>
2022-08-29 21:04:16 -0700

    added useEffect for fetching data on load

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-export { Navbar, Footer };
+import HorizontalScrollbar from "./HorizontalScrollbar";
+export { Navbar, Footer, HorizontalScrollbar };
